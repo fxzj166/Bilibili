644eb3a2d8c86004660de154af8944f8e3b29682
fxzj166 <63585940+fxzj166@users.noreply.github.com>
2020-04-13 18:18:25 +0800

    这个是执行程序
    这个是执行程序，通过这个程序调用和循环主程序，实现粉丝数的刷新

---
+++ bilibili.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#- * -coding: utf - 8 - * -
+#sudo ntpdate cn.pool.ntp.org 添加到/etc/rc.local里面实现开机自启动，同步网络时间
+
+import os
+import time, datetime
+def sleeptime(hour,min,sec):
+    return hour*3600 + min*60 + sec;
+while 1==1:
+    os.system("python /home/pi/rj/python/examples/bilibili01.py")
+    second = sleeptime(0,30,0)  #刷新间隔，小时，分钟，秒，现在设置30分钟刷新一次
+    time.sleep(second)
+
