31c59c9f502f16cf7eba5461a5658446b6f70849
fxzj166 <63585940+fxzj166@users.noreply.github.com>
2020-04-13 19:05:33 +0800

    Update bilibili.py

--- bilibili.py
+++ bilibili.py
@@ -7,7 +7,7 @@ import time, datetime
 def sleeptime(hour,min,sec):
     return hour*3600 + min*60 + sec;
 while 1==1:
-    os.system("python /home/pi/rj/python/examples/bilibili01.py")
+    os.system("python /home/pi/bilibili01.py")
     second = sleeptime(0,30,0)  #刷新间隔，小时，分钟，秒，现在设置30分钟刷新一次
     time.sleep(second)
 
